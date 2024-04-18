4c741bd0bbc8d4d602b63910e6635e91dc7f38f8
work.ducmanh0323 <work.ducmanh0323@gmail.com>
2024-04-18 22:06:39 +0700

    Update chat history

--- app.py
+++ app.py
@@ -16,11 +16,29 @@ output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
 
 st.title("Demo")
-st.chat_message("assistant").write("Hello, can I help you!")
-if prompt := st.chat_input():
-    st.chat_message("user").write(prompt)
 
-    response = chain.invoke({"input": prompt})
-    print(response)
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Can I help you you?"},
+    ]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
     with st.chat_message("assistant"):
+        response = chain.invoke({"input": prompt})
         st.write(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
